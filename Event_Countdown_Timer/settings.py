23d6e4a478350894e2156d25e7980b81e78ef35a
nandanapbtech-bit <nandanapb.tech@gmail.com>
2026-08-17 23:41:14 +0530

    Fix Render allowed hosts

--- Event_Countdown_Timer/settings.py
+++ Event_Countdown_Timer/settings.py
@@ -15,11 +15,11 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
-).split(",")
-
+ALLOWED_HOSTS = [
+    "event-countdown-timer-rs4f.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 INSTALLED_APPS = [
